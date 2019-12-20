364b10b47dcf70627baf8750d3eb41b772594ba5
ninjapretzel <ninjapretzel@yahoo.com>
2019-12-20 01:06:42 -0700

    They were `bool`s, before, not `int`s.

--- PixelEngine/Input.cs
+++ PixelEngine/Input.cs
@@ -2,7 +2,7 @@ namespace PixelEngine {
 	/// <summary> Struct representing a snapshot of the state of an input key. </summary>
 	public struct Input {
 		/// <summary> Data byte... Still a bit wasteful, but less than before. </summary>
-		/// <remarks> With 3 bool fields, an Input was 12 bytes before, now it is just one. </remarks>
+		/// <remarks> With 3 bool fields, an Input was 3 bytes before, now it is just one. </remarks>
 		private byte data;
 		/// <summary> Mask for pressed bit </summary>
 		private const byte PRESSED = 0x01;
